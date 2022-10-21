45fd4833f27090c86d6f73870092896700347ef0
Ines Diaz Funes <inesdiazfunes123@gmail.com>
2022-10-20 21:56:58 -0300

    second commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+let usuario1 = "ine";
+let usuario2 = "diaz";
+
+let nombreUsuario = prompt("Ingrese nombre de usuario");
+
+if (nombreUsuario == "ine" || nombreUsuario == "diaz") {
+    alert("Bienvenido " + nombreUsuario);
+} else{
+    alert("Nombre de usuario incorrecto");
+}
+
+// for (let i = 0; i < 5; i++) {
+//     console.log("Ciclo numero " + i);
+//     for (let p = 0; p < 2; p++) {
+//        console.log("ciclo secun" + p);
+//     }
+// }
+
+// alert("Bienvenido a la calculadora. Para finalizar, escriba 'final' luego de la operacion.  Buena suerte!");
+
+// let inicio = "inicio";
+// let final = "final";
+
+// while (inicio == "inicio"){
+
+//     let suma = Number(prompt(""))
+
+//     prompt("¿Desea finalizar?")
+//     if(final == "final"){
+//         let inicio = "final";
+//     }
+
+// }
+
+
