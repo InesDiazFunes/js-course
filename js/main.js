b56fa7b2eeebcc711789dd1d1b608fbe4929762c
Ines Diaz Funes <inesdiazfunes123@gmail.com>
2022-10-20 23:58:40 -0300

    im stupid

--- js/main.js
+++ js/main.js
@@ -1,58 +1,58 @@
-// alert("Bienvenido a la calculadura. Dos numeros para cuatro cuentas. Siga las instrucciones para recibir a cambio los resultados de las operaciones.");
+alert("Bienvenido a la calculadura. Dos numeros para cuatro cuentas. Siga las instrucciones para recibir a cambio los resultados de las operaciones.");
 
-// let inicializar = true;
+let inicializar = true;
 
-// while (inicializar) {
+while (inicializar) {
 
-//     let numero1 = Number(prompt("Ingrese un numero"));
-//     let numero2 = Number(prompt("Ingrese otro numero"));
+    let numero1 = Number(prompt("Ingrese un numero"));
+    let numero2 = Number(prompt("Ingrese otro numero"));
     
-//     let sumar = prompt("Escriba 'sumar' para sumar");
-//     let restar = prompt("Escriba 'restar' para restar");
-//     let dividir = prompt("Escriba 'dividir' para dividir");
-//     let multiplicar = prompt ("Escriba 'multiplicar' para multiplicar");
+    let sumar = prompt("Escriba 'sumar' para sumar");
+    let restar = prompt("Escriba 'restar' para restar");
+    let dividir = prompt("Escriba 'dividir' para dividir");
+    let multiplicar = prompt ("Escriba 'multiplicar' para multiplicar");
 
-//         function suma ( numero1, numero2){
-//             let result = numero1 + numero2;
-//             return result;
-//         } 
-//         function resta ( numero1, numero2){
-//             let result = numero1 - numero2;
-//             return result;
-//         }
-//         function division ( numero1, numero2){
-//             let result = numero1 / numero2;
-//             return result;
-//         } 
-//         function multiplicacion ( numero1, numero2){
-//             let result = numero1 * numero2;
-//             return result;
-//         }
+        function suma ( numero1, numero2){
+            let result = numero1 + numero2;
+            return result;
+        } 
+        function resta ( numero1, numero2){
+            let result = numero1 - numero2;
+            return result;
+        }
+        function division ( numero1, numero2){
+            let result = numero1 / numero2;
+            return result;
+        } 
+        function multiplicacion ( numero1, numero2){
+            let result = numero1 * numero2;
+            return result;
+        }
 
-//             if(sumar == "sumar"){  
-//                 console.log(suma(numero1, numero2)); 
-//                 alert(numero1 + "+" + numero2 + "=" + suma(numero1,numero2));
-//             }
-//             if (restar == "restar"){
-//                 console.log(resta(numero1,numero2));
-//                 alert(numero1 + "-" + numero2 + "=" + resta(numero1,numero2));
-//             }
-//             if(dividir == "dividir"){
-//                 console.log(division(numero1,numero2));
-//                 alert(numero1 + "/" + numero2 + "=" + division(numero1,numero2));
+            if(sumar == "sumar"){  
+                console.log(suma(numero1, numero2)); 
+                alert(numero1 + "+" + numero2 + "=" + suma(numero1,numero2));
+            }
+            if (restar == "restar"){
+                console.log(resta(numero1,numero2));
+                alert(numero1 + "-" + numero2 + "=" + resta(numero1,numero2));
+            }
+            if(dividir == "dividir"){
+                console.log(division(numero1,numero2));
+                alert(numero1 + "/" + numero2 + "=" + division(numero1,numero2));
 
-//             }
-//             if(multiplicar == "multiplicar"){
-//                 console.log(multiplicacion(numero1,numero2));
-//                 alert(numero1 + "*" + numero2 + "=" + multiplicacion(numero1,numero2));
-//             }
+            }
+            if(multiplicar == "multiplicar"){
+                console.log(multiplicacion(numero1,numero2));
+                alert(numero1 + "*" + numero2 + "=" + multiplicacion(numero1,numero2));
+            }
 
-//             let finalizar = prompt("¿Desea finalizar? Escriba 'final'.");
+            let finalizar = prompt("¿Desea finalizar? Escriba 'final'.");
 
-//             if (finalizar == "final") {
-//                 alert("Gracias por utilizar la calculadora.")
-//                 break;
-//             } else {
-//                 continue;
-//             }
-// }
+            if (finalizar == "final") {
+                alert("Gracias por utilizar la calculadora.")
+                break;
+            } else {
+                continue;
+            }
+}
