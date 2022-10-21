f22fd4e1838f9b86d124d2d0925647f9520fccc0
Ines Diaz Funes <inesdiazfunes123@gmail.com>
2022-10-20 23:52:39 -0300

    calcu reparada con funcs

--- js/main.js
+++ js/main.js
@@ -1,35 +1,58 @@
-let usuario1 = "ine";
-let usuario2 = "diaz";
+alert("Bienvenido a la calculadura. Dos numeros para cuatro cuentas. Siga las instrucciones para recibir a cambio los resultados de las operaciones.");
 
-let nombreUsuario = prompt("Ingrese nombre de usuario");
+let inicializar = true;
 
-if (nombreUsuario == "ine" || nombreUsuario == "diaz") {
-    alert("Bienvenido " + nombreUsuario);
-} else{
-    alert("Nombre de usuario incorrecto");
+while (inicializar) {
+
+    let numero1 = Number(prompt("Ingrese un numero"));
+    let numero2 = Number(prompt("Ingrese otro numero"));
+    
+    let sumar = prompt("Escriba 'sumar' para sumar");
+    let restar = prompt("Escriba 'restar' para restar");
+    let dividir = prompt("Escriba 'dividir' para dividir");
+    let multiplicar = prompt ("Escriba 'multiplicar' para multiplicar");
+
+        function suma ( numero1, numero2){
+            let result = numero1 + numero2;
+            return result;
+        } 
+        function resta ( numero1, numero2){
+            let result = numero1 - numero2;
+            return result;
+        }
+        function division ( numero1, numero2){
+            let result = numero1 / numero2;
+            return result;
+        } 
+        function multiplicacion ( numero1, numero2){
+            let result = numero1 * numero2;
+            return result;
+        }
+
+            if(sumar == "sumar"){  
+                console.log(suma(numero1, numero2)); 
+                alert(numero1 + "+" + numero2 + "=" + suma(numero1,numero2));
+            }
+            if (restar == "restar"){
+                console.log(resta(numero1,numero2));
+                alert(numero1 + "-" + numero2 + "=" + resta(numero1,numero2));
+            }
+            if(dividir == "dividir"){
+                console.log(division(numero1,numero2));
+                alert(numero1 + "/" + numero2 + "=" + division(numero1,numero2));
+
+            }
+            if(multiplicar == "multiplicar"){
+                console.log(multiplicacion(numero1,numero2));
+                alert(numero1 + "*" + numero2 + "=" + multiplicacion(numero1,numero2));
+            }
+
+            let finalizar = prompt("¿Desea finalizar? Escriba 'final'.");
+
+            if (finalizar == "final") {
+                alert("Gracias por utilizar la calculadora.")
+                break;
+            } else {
+                continue;
+            }
 }
-
-// for (let i = 0; i < 5; i++) {
-//     console.log("Ciclo numero " + i);
-//     for (let p = 0; p < 2; p++) {
-//        console.log("ciclo secun" + p);
-//     }
-// }
-
-// alert("Bienvenido a la calculadora. Para finalizar, escriba 'final' luego de la operacion.  Buena suerte!");
-
-// let inicio = "inicio";
-// let final = "final";
-
-// while (inicio == "inicio"){
-
-//     let suma = Number(prompt(""))
-
-//     prompt("¿Desea finalizar?")
-//     if(final == "final"){
-//         let inicio = "final";
-//     }
-
-// }
-
-
