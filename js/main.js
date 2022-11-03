9bc9290710adba80ad6c4e6e8931141c685552fd
Ines Diaz Funes <inesdiazfunes123@gmail.com>
2022-11-03 18:39:23 -0300

    bs

---
+++ js/main.js
@@ -0,0 +1,69 @@
+// function Producto(titulo, color, precio){
+//     this.titulo = titulo;
+//     this.color = color;
+//     this.precio = precio;
+// }
+
+// const producto1 = new Producto("Sillon", "Rojo", 50000);
+// const producto2 = new Producto("Cama", "Blanco", 160000);
+
+// console.log(producto2);
+
+// class Producto {
+//     constructor(titulo, color, precio) {
+//         this.titulo = titulo;
+//         this.color = color;
+//         this.precio = precio;
+//     }
+// }
+
+// const producto1 = new Producto("Silloncito", "Rojo", 5000);
+// console.log(producto1);
+
+// class Persona {
+//     constructor(nombre, edad, nacionalidad) {
+//         this.nombre = nombre;
+//         this.edad = edad;
+//         this.nacionalidad = nacionalidad;
+//         this.saludar = function(){console.log("Hola, soy " + this.nombre)}
+//     }
+// }
+
+// const persona1 = new Persona("Ine", 20, "Argentina");
+// console.log(persona1);
+// persona1.saludar();
+
+// const array = [];
+
+// const numeros = [1,2,3,4,5];
+// const booleanos = [true, false, true];
+// const mezcla = ["Sillon", 1 , 2, true];
+
+// "Armario", "Escritorio", "Cama", "Puerta"
+
+const productos = ["Sillon", "Silla", "Mesa", "Ropero"];
+// console.log(productos);
+// console.log(productos[2]);
+// for (let i = 0; i < 4 ; i++){
+//     console.log(productos[i]);
+// }
+    
+// for (let i = 0; i < productos.length ; i++){
+//     console.log(productos[i]);
+//         }
+// console.log(productos.length);
+
+// productos.push("Armario"); //(Agrear un elemento al final del Array)
+// productos.unshift("Armario"); //(Agregar  un elemento al principio del Array)
+// productos.shift(); //(Eliminar un elemento al principio del Array)
+// productos.pop(); //(Eliminar un elemento al final del Array)
+// productos.splice(1, 1); //(Eliminar cualquier elemento segun posicion en Array)
+//console.log(productos.join(", ")); // (Generar una string con los elementos del Array)
+
+// const producto2 = ["Puerta", "Ventana", "Cama"];
+// const productoFinal = productos.concat(producto2); //(Unir dos Arrays)
+
+//const productosComedor = productos.slice(0, 2); // ()
+
+console.log(productosComedor);
+
